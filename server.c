e42b6f166e53ec46db1caef220bff2656fa71a8f
Maicon-g14 <maicon_gabriel12@hotmail.com>
2024-05-12 10:16:07 -0300

    chore: server modularization and error handling

--- server.c
+++ server.c
@@ -16,54 +16,59 @@
 #define PORT "3490"
 #define BACKLOG 10
 
-void reap_dead_children(int signal)
-{
+void reap_dead_children(int signal) {
     while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-void *get_socket_address(struct sockaddr *sa)
-{
+void setup_sigchld_handling() {
+    struct sigaction signal_action;
+    signal_action.sa_handler = reap_dead_children;
+    sigemptyset(&signal_action.sa_mask);
+    
+    signal_action.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGCHLD, &signal_action, NULL) == -1) {
+        perror("sigaction failed");
+        exit(1);
+    }
+}
+
+void *get_socket_address(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
-{
-    int server_socket = -1, client_socket = -1;
-    struct addrinfo hints, *server_info, *p;
-    struct sockaddr_storage client_address;
-    socklen_t address_size;
-    struct sigaction signal_action;
-    int option = 1;
-    char client_ip[INET6_ADDRSTRLEN] = "\0";
-    int rv = -1;
+int bind_socket(int sock_type, int opt_name, int *option) {
+    int server_socket = -1, gai_rv = -1;
+    struct addrinfo addr_init, *server_info, *curr_server_info;
+    memset(&addr_init, 0, sizeof addr_init);
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    addr_init.ai_family = AF_UNSPEC;        // use IPv4 or IPv6, whichever
+    addr_init.ai_socktype = sock_type;      
+    addr_init.ai_flags = AI_PASSIVE;        // fill in host IP
 
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &server_info)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    if ((gai_rv = getaddrinfo(NULL, PORT, &addr_init, &server_info)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(gai_rv));
         return 1;
     }
 
-    for(p = server_info; p != NULL; p = p->ai_next) {
-        if ((server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("server: socket");
+    // bind to socket
+    for (curr_server_info = server_info; curr_server_info != NULL; curr_server_info = curr_server_info->ai_next) {
+        if ((server_socket = socket(curr_server_info->ai_family, curr_server_info->ai_socktype, curr_server_info->ai_protocol)) == -1) {
+            perror("server: socket error");
             continue;
         }
         
-        if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int)) == -1) {
-            perror("setsockopt");
+        if (setsockopt(server_socket, SOL_SOCKET, opt_name, option, sizeof(int)) == -1) {
+            perror("server: setsockopt error");
             exit(1);
         }
 
-        if (bind(server_socket, p->ai_addr, p->ai_addrlen) == -1) {
+        if (bind(server_socket, curr_server_info->ai_addr, curr_server_info->ai_addrlen) == -1) {
             close(server_socket);
-            perror("server: bind");
+            perror("server: couldn't bind to address");
             continue;
         }
         break;
@@ -71,23 +76,34 @@ int main(void)
 
     freeaddrinfo(server_info);
 
-    if (p == NULL)  {
+    if (curr_server_info == NULL)  {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
     if (listen(server_socket, BACKLOG) == -1) {
-        perror("listen");
+        perror("server: failed to listen");
         exit(1);
     }
 
-    signal_action.sa_handler = reap_dead_children;
-    sigemptyset(&signal_action.sa_mask);
-    signal_action.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &signal_action, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
+    return server_socket;
+}
+
+int main(void) {
+    int client_socket = -1, option = 1;
+    socklen_t address_size = -1;
+    struct sockaddr_storage client_address;
+
+    char client_ip[INET6_ADDRSTRLEN];
+    memset(client_ip, '\0', sizeof(client_ip));
+
+    // tcp socket
+    int server_socket = bind_socket(SOCK_STREAM, SO_REUSEADDR, &option);
+    
+    // udp socket
+    // int server_socket = bind_socket(SOCK_DGRAM, SOCK_DGRAM, &option);
+
+    setup_sigchld_handling();
 
     printf("server: waiting for connections...\n");
 
@@ -99,13 +115,19 @@ int main(void)
             continue;
         }
 
+         // Show client's IP
         inet_ntop(client_address.ss_family, get_socket_address((struct sockaddr *)&client_address), client_ip, sizeof client_ip);
         printf("server: got connection from %s\n", client_ip);
 
-        if (!fork()) {
+        if (fork() == 0) {
             close(server_socket);
             char admin_flag;
-            recv(client_socket, &admin_flag, 1, 0); // Receives the client's flag
+
+            if (recv(client_socket, &admin_flag, sizeof admin_flag, 0) <= 0) {
+                perror("server: error receiving client's flag");
+                close(client_socket);
+                continue;
+            }
 
             if (admin_flag == '1') {
                 serve_client_admin(client_ip, client_socket);
@@ -114,7 +136,6 @@ int main(void)
             }
 
             close(client_socket);
-            exit(0);
         }
         close(client_socket);
     }
